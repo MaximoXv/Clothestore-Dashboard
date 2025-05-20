2c609b0590f6e655a995fcf8d42a76469e1a9d63
MaximoXv <maximoibarra164@gmail.com>
2025-05-20 11:03:39 -0300

    Feat: fetching data from backend

--- src/pages/dashboard-page.tsx
+++ src/pages/dashboard-page.tsx
@@ -1,7 +1,7 @@
 import { Table, Tag } from "antd";
 import ReactApexChart from "react-apexcharts";
 import { clx } from "../utils/clx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "../components/header";
 import Section from "../components/section";
 
@@ -40,35 +40,84 @@ interface DashboardPageProps {
   changeStateNavbar: () => void;
 }
 
+interface Sale {
+  key: number;
+  image: string;
+  name: string;
+  model: string;
+  branch: string;
+  price: number;
+  status: string;
+}
+
+interface AgeCategories {
+  id: number;
+  name: string;
+  productCount: number;
+}
+
+interface BranchSales {
+  branchId: number;
+  branchName: string;
+  salesCount: number[];
+}
+
+interface Totals {
+  totalProducts: number;
+  totalUsers: number;
+  totalCategories: number;
+}
+
+interface LastProduct {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  branch: string;
+  image: string;
+  colors: string[];
+  season: string;
+  age: string;
+  genre: string;
+}
+
 const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
-  const [products, setProducts] = useState([
-    {
-      key: "1",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Realizado",
-    },
-    {
-      key: "2",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Pendiente",
-    },
-    {
-      key: "3",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Cancelado",
-    },
-  ]);
+  const [sales, setSales] = useState<Sale[]>([]);
+  const [lastProduct, setLastProduct] = useState<LastProduct>({
+    id: 0,
+    name: "",
+    description: "",
+    image: "",
+    price: 0,
+    branch: "",
+    age: "",
+    genre: "",
+    season: "",
+    colors: [],
+  });
+  const [ageCategories, setAgeCategories] = useState<AgeCategories[]>([]);
+  const [branchSales, setBranchSales] = useState<BranchSales[]>([]);
+  const [totals, setTotals] = useState<Totals>({
+    totalProducts: 0,
+    totalUsers: 0,
+    totalCategories: 0,
+  });
 
   const columns = [
+    {
+      title: "Imagen",
+      dataIndex: "image",
+      key: "image",
+      // fixed: "left",
+      render: (_, { image }) => {
+        return (
+          <img
+            className="h-12 w-12 object-cover rounded-[50%]"
+            src={`http://localhost:3000/images/${image}`}
+          />
+        );
+      },
+    },
     {
       title: "Nombre",
       dataIndex: "name",
@@ -89,7 +138,6 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
       title: "Precio",
       dataIndex: "price",
       key: "price",
-      // fixed: "right",
     },
     {
       title: "Estado",
@@ -105,7 +153,6 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
             : status == "Cancelado"
             ? "!text-blue-400 !border-0 !bg-blue-200"
             : "!text-sky-400 !border-0 !bg-sky-200";
-        console.log(tag);
 
         return (
           <Tag className={myClassName} key={status}>
@@ -115,6 +162,88 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
       },
     },
   ];
+
+  useEffect(() => {
+    const fetchSales = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/sales");
+        const data = await response.json();
+        const saleArray = data.map((sale) => {
+          const auxSale: Sale = {
+            key: sale.id,
+            image: sale.product.Images[0].url,
+            name: sale.product.name,
+            model: sale.product.model,
+            branch: sale.product.branch.name,
+            price: sale.price,
+            status: sale.status,
+          };
+          return auxSale;
+        });
+        setSales(saleArray);
+      } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+      }
+    };
+    const fetchAgeCategories = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/ageCategories");
+        const data = await response.json();
+
+        setAgeCategories(data);
+      } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+      }
+    };
+    const fetchBranchSales = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/branchSales");
+        const data = await response.json();
+
+        setBranchSales(data);
+      } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+      }
+    };
+    const fetchTotals = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/getTotals");
+        const data = await response.json();
+
+        setTotals(data);
+      } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+      }
+    };
+
+    const fetchLastProduct = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/lastProduct");
+        const data = await response.json();
+        const lastProd: LastProduct = {
+          id: data.id,
+          name: data.name,
+          description: data.description,
+          image: data.Images[0].url,
+          price: data.price,
+          branch: data.branch.name,
+          age: data.age.name,
+          genre: data.genre.name,
+          season: data.season.name,
+          colors: data.Colors.map((color) => color.value),
+        };
+        setLastProduct(lastProd);
+      } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+      }
+    };
+
+    fetchSales();
+    fetchBranchSales();
+    fetchAgeCategories();
+    fetchTotals();
+    fetchLastProduct();
+  }, []);
   return (
     <>
       <main className="w-4/5 bg-gray-100 gap-4 p-6 flex flex-col overflow-auto max-lg:w-full">
@@ -133,14 +262,14 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
               iconBgColor="bg-violet-200"
               icon="fa-shopping-cart"
               label="Productos totales"
-              value="20000"
+              value={totals.totalProducts.toString()}
             />
             <SectionPanel
               textColor="text-indigo-400"
               iconBgColor="bg-indigo-200"
               icon="fa-users"
               label="Usuarios totales"
-              value="20000"
+              value={totals.totalUsers.toString()}
             />
           </div>
           <div
@@ -151,14 +280,7 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
               iconBgColor="bg-blue-200"
               icon="fa-box-archive"
               label="Categorias totales"
-              value="7"
-            />
-            <SectionPanel
-              textColor="text-sky-400"
-              iconBgColor="bg-sky-200"
-              icon="fa-box-archive"
-              label="Categorias totales"
-              value="7"
+              value={totals.totalCategories.toString()}
             />
           </div>
         </div>
@@ -170,16 +292,20 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
                   type="area"
                   height={"100%"}
                   width={"100%"}
-                  series={[
-                    {
-                      name: "Nike",
-                      data: [31, 40, 28],
-                    },
-                    {
-                      name: "Puma",
-                      data: [11, 32, 45],
-                    },
-                  ]}
+                  // series={[
+                  //   {
+                  //     name: "Nike",
+                  //     data: [31, 40, 28],
+                  //   },
+                  //   {
+                  //     name: "Puma",
+                  //     data: [11, 32, 45],
+                  //   },
+                  // ]}
+                  series={branchSales.map((branch) => ({
+                    name: branch.branchName,
+                    data: branch.salesCount,
+                  }))}
                   options={{
                     // responsive: [
                     //   { breakpoint: 1000, options: { width: 300 } },
@@ -200,13 +326,6 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
                         "Marzo",
                         "Abril",
                         "Mayo",
-                        "Junio",
-                        "Julio",
-                        "Agosto",
-                        "Septiembre",
-                        "Octubre",
-                        "Noviembre",
-                        "Diciembre",
                       ],
                       type: "category",
                     },
@@ -239,7 +358,7 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
                 scroll={{ x: "max-content" }}
                 bordered
                 className="mt-2"
-                dataSource={products}
+                dataSource={sales}
                 columns={columns}
               />
             </Section>
@@ -250,7 +369,7 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
                 <ReactApexChart
                   type="donut"
                   height={"100%"}
-                  series={[40, 100, 130, 12, 30]}
+                  series={ageCategories.map((age) => age.productCount)}
                   options={{
                     colors: [
                       "#a78bfa",
@@ -260,7 +379,7 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
                       "#34d399",
                       // "#4ade80",
                     ],
-                    labels: ["dia 1", "dia 2", "dia 3", "dia 4", "dia 5"],
+                    labels: ageCategories.map((age) => age.name),
                     plotOptions: {
                       pie: {
                         donut: {
@@ -294,40 +413,49 @@ const DashboardPage = ({ changeStateNavbar }: DashboardPageProps) => {
                 <div className="w-full bg-gray-200 flex justify-center relative">
                   <img
                     className="max-h-48"
-                    src="https://vcp.com.ar/cdn/shop/files/CurbiNegra2.jpg?v=1747147261&width=700"
+                    src={`http://localhost:3000/images/${lastProduct.image}`}
                     alt=""
                   />
                   <div className="absolute flex flex-col right-0 top-0 p-4 gap-2">
-                    <span className="w-6 h-6 bg-blue-400 rounded-[50%] border-[2px] border-black"></span>
-                    <span className="w-6 h-6 bg-gray-400 rounded-[50%] border-[2px] border-black"></span>
-                    <span className="w-6 h-6 bg-black rounded-[50%] border-[2px] border-black"></span>
+                    {lastProduct.colors.map((color) => {
+                      const auxColor =
+                        color == "#181818"
+                          ? "!bg-black"
+                          : color == "#f3f3f3"
+                          ? "!bg-white"
+                          : color == "#b8b8b8"
+                          ? "!bg-zinc-400"
+                          : color == "#4177eb"
+                          ? "!bg-blue-500"
+                          : "!bg-white";
+                      return (
+                        <span
+                          className={`!w-6 !h-6 !rounded-[50%] !border-[2px] !border-black ${auxColor}`}
+                        ></span>
+                      );
+                    })}
                   </div>
                   <div className="absolute flex flex-col right-[50%] translate-1/2 bottom-0 p-4 gap-2">
                     <span className="bg-violet-200 text-violet-400 px-3 py-1 rounded-lg border-[1px] border-violet-400 font-bold">
-                      Puma
+                      {lastProduct.branch}
                     </span>
                   </div>
                 </div>
                 <div className="flex flex-col p-4">
                   <div className="w-full flex justify-between items-center flex-wrap">
-                    <h4 className="font-bold text-xl">
-                      Remera deportiva de verano
-                    </h4>
-                    <h4 className="font-bold text-xl">$4000</h4>
+                    <h4 className="font-bold text-xl">{lastProduct.name}</h4>
+                    <h4 className="font-bold text-xl">${lastProduct.price}</h4>
                   </div>
-                  <p className="text-sm">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Neque, mollitia velit quae optio ad vitae?
-                  </p>
+                  <p className="text-sm">{lastProduct.description}</p>
                   <div className="w-full flex mt-2 gap-1 flex-wrap justify-center">
                     <span className="text-sm py-1 px-3 rounded-lg border-[1px] border-indigo-400 text-indigo-400 bg-indigo-200">
-                      Verano
+                      {lastProduct.season}
                     </span>
                     <span className="text-sm py-1 px-3 rounded-lg border-[1px] border-indigo-400 text-indigo-400 bg-indigo-200">
-                      Adulto
+                      {lastProduct.age}
                     </span>
                     <span className="text-sm py-1 px-3 rounded-lg border-[1px] border-indigo-400 text-indigo-400 bg-indigo-200">
-                      Masculino
+                      {lastProduct.genre}
                     </span>
                   </div>
                 </div>
