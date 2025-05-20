370f51cd09371f39fff74cd1a0840f86185eade9
MaximoXv <maximoibarra164@gmail.com>
2025-05-20 06:53:24 -0300

    Feat: implement fetch to clients and products page

--- src/pages/clients-page.tsx
+++ src/pages/clients-page.tsx
@@ -1,87 +1,117 @@
 import { Table, Tag } from "antd";
 import Header from "../components/header";
 import Section from "../components/section";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface ClientsPageProps {
   changeStateNavbar: () => void;
 }
 
+interface User {
+  key: number;
+  fullname: string;
+  direction: string;
+  phonenumber: number;
+  email: string;
+  avatar: string;
+  role: string;
+}
+
 const ClientsPage = ({ changeStateNavbar }: ClientsPageProps) => {
-  const [products, setProducts] = useState([
-    {
-      key: "1",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Realizado",
-    },
-    {
-      key: "2",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Pendiente",
-    },
-    {
-      key: "3",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Cancelado",
-    },
-  ]);
+  const [users, setUsers] = useState<User[]>([]);
 
   const columns = [
     {
-      title: "Nombre",
-      dataIndex: "name",
-      key: "name",
+      title: "Imagen",
+      dataIndex: "avatar",
+      key: "avatar",
       // fixed: "left",
-    },
-    {
-      title: "Modelo",
-      dataIndex: "model",
-      key: "model",
-    },
-    {
-      title: "Marca",
-      dataIndex: "branch",
-      key: "branch",
-    },
-    {
-      title: "Precio",
-      dataIndex: "price",
-      key: "price",
-      // fixed: "right",
-    },
-    {
-      title: "Estado",
-      dataIndex: "status",
-      key: "status",
-      // fixed: "right",
-      render: (tag, { status }) => {
-        const myClassName =
-          status == "Realizado"
-            ? "!text-violet-400 !border-0 !bg-violet-200"
-            : status == "Pendiente"
-            ? "!text-indigo-400 !border-0 !bg-indigo-200"
-            : status == "Cancelado"
-            ? "!text-blue-400 !border-0 !bg-blue-200"
-            : "!text-sky-400 !border-0 !bg-sky-200";
-        console.log(tag);
-
+      render: (_, { avatar }) => {
         return (
-          <Tag className={myClassName} key={status}>
-            {status.toUpperCase()}
-          </Tag>
+          <img
+            className="h-12 w-12 object-cover rounded-[50%]"
+            src={`http://localhost:3000/images/users/${avatar}`}
+          />
         );
       },
     },
+    {
+      title: "Nombre",
+      dataIndex: "fullname",
+      key: "fullname",
+      // fixed: "left",
+    },
+    {
+      title: "Dirección",
+      dataIndex: "direction",
+      key: "direction",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Télefono",
+      dataIndex: "phonenumber",
+      key: "phonenumber",
+      // fixed: "right",
+    },
+    {
+      title: "Rol",
+      dataIndex: "role",
+      key: "role",
+      // fixed: "right",
+      render: (_, { role }) => {
+        const myClassName =
+          role == "ADMIN"
+            ? "!text-violet-400 !border-0 !bg-violet-200"
+            : role == "CLIENT"
+            ? "!text-indigo-400 !border-0 !bg-indigo-200"
+            : role == "SELLER"
+            ? "!text-blue-400 !border-0 !bg-blue-200"
+            : "!text-sky-400 !border-0 !bg-sky-200";
+
+        const auxRole =
+          role == "ADMIN"
+            ? "Administrador"
+            : role == "CLIENT"
+            ? "Cliente"
+            : role == "SELLER"
+            ? "Vendedor"
+            : "Desconocido";
+
+        return <Tag className={myClassName}>{auxRole.toUpperCase()}</Tag>;
+      },
+    },
   ];
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/users");
+        const data = await response.json();
+        console.log(data);
+        const userArray = data.map((user) => {
+          const auxUser: User = {
+            key: user.id,
+            avatar: user.avatar,
+            fullname: user.fullname,
+            direction: user.direction,
+            email: user.email,
+            phonenumber: user.phonenumber,
+            role: user.role.name,
+          };
+          return auxUser;
+        });
+        setUsers(userArray);
+      } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
   return (
     <main className="w-4/5 min-h-screen bg-gray-100 gap-4 p-6 flex flex-col overflow-auto max-lg:w-full">
       <Header
@@ -95,7 +125,7 @@ const ClientsPage = ({ changeStateNavbar }: ClientsPageProps) => {
           scroll={{ x: "max-content" }}
           bordered
           className="mt-2"
-          dataSource={products}
+          dataSource={users}
           columns={columns}
         />
       </Section>
--- src/pages/products-page.tsx
+++ src/pages/products-page.tsx
@@ -1,41 +1,44 @@
 import { Table, Tag } from "antd";
 import Header from "../components/header";
 import Section from "../components/section";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface ProductsPageProps {
   changeStateNavbar: () => void;
 }
 
+interface Product {
+  key: number;
+  name: string;
+  description: string;
+  model: string;
+  price: number;
+  stock: number;
+  branch: string;
+  categories: string[];
+  image: string;
+  colors: string[];
+  sizes: string[];
+}
+
 const ProductsPage = ({ changeStateNavbar }: ProductsPageProps) => {
-  const [products, setProducts] = useState([
-    {
-      key: "1",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Realizado",
-    },
-    {
-      key: "2",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Pendiente",
-    },
-    {
-      key: "3",
-      name: "Zapatillas Flac Deportivas",
-      model: "LANA033",
-      branch: "Puma",
-      price: "$10000",
-      status: "Cancelado",
-    },
-  ]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   const columns = [
+    {
+      title: "Imagen",
+      dataIndex: "image",
+      key: "image",
+      // fixed: "left",
+      render: (_, { image }) => {
+        return (
+          <img
+            className="h-12 w-12 object-cover rounded-[50%]"
+            src={`http://localhost:3000/images/${image}`}
+          />
+        );
+      },
+    },
     {
       title: "Nombre",
       dataIndex: "name",
@@ -43,9 +46,24 @@ const ProductsPage = ({ changeStateNavbar }: ProductsPageProps) => {
       // fixed: "left",
     },
     {
+      title: "Descripción",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
       title: "Modelo",
       dataIndex: "model",
       key: "model",
+    },
+    {
+      title: "Precio",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Stock",
+      dataIndex: "stock",
+      key: "stock",
     },
     {
       title: "Marca",
@@ -53,35 +71,91 @@ const ProductsPage = ({ changeStateNavbar }: ProductsPageProps) => {
       key: "branch",
     },
     {
-      title: "Precio",
-      dataIndex: "price",
-      key: "price",
+      title: "Categorias",
+      dataIndex: "categories",
+      key: "categories",
       // fixed: "right",
+      render: (_, { categories }) => {
+        const myClassName = "!text-violet-400 !border-0 !bg-violet-200";
+
+        return categories.map((category) => (
+          <Tag className={myClassName}>{category.toUpperCase()}</Tag>
+        ));
+      },
     },
     {
-      title: "Estado",
-      dataIndex: "status",
-      key: "status",
+      title: "Colores",
+      dataIndex: "colors",
+      key: "colors",
       // fixed: "right",
-      render: (tag, { status }) => {
-        const myClassName =
-          status == "Realizado"
-            ? "!text-violet-400 !border-0 !bg-violet-200"
-            : status == "Pendiente"
-            ? "!text-indigo-400 !border-0 !bg-indigo-200"
-            : status == "Cancelado"
-            ? "!text-blue-400 !border-0 !bg-blue-200"
-            : "!text-sky-400 !border-0 !bg-sky-200";
-        console.log(tag);
+      render: (_, { colors }) => {
+        return colors.map((color) => {
+          const auxColor =
+            color == "#181818"
+              ? "!bg-black"
+              : color == "#f3f3f3"
+              ? "!bg-white"
+              : color == "#b8b8b8"
+              ? "!bg-zinc-400"
+              : color == "#4177eb"
+              ? "!bg-blue-500"
+              : "!bg-white";
+          return (
+            <Tag
+              className={`!w-6 !h-6 !rounded-[50%] !border-[2px] !border-black ${auxColor}`}
+            ></Tag>
+          );
+        });
+      },
+    },
+    {
+      title: "Tamaños",
+      dataIndex: "sizes",
+      key: "sizes",
+      // fixed: "right",
+      render: (_, { sizes }) => {
+        const myClassName = "!text-violet-400 !border-0 !bg-violet-200";
 
-        return (
-          <Tag className={myClassName} key={status}>
-            {status.toUpperCase()}
-          </Tag>
-        );
+        return sizes.map((size) => (
+          <Tag className={myClassName}>{size.toUpperCase()}</Tag>
+        ));
       },
     },
   ];
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/products");
+        const data = await response.json();
+        const productArray = data.map((product) => {
+          const auxProduct: Product = {
+            key: product.id,
+            name: product.name,
+            description: product.description,
+            model: product.model,
+            price: product.price,
+            stock: product.stock,
+            branch: product.branch.name,
+            categories: [
+              product.season.name,
+              product.age.name,
+              product.genre.name,
+            ],
+            image: product.Images[0].url,
+            colors: product.Colors.map((color) => color.value),
+            sizes: product.Sizes.map((size) => size.name),
+          };
+          return auxProduct;
+        });
+        setProducts(productArray);
+      } catch (error) {
+        console.error("Error al obtener los usuarios:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   return (
     <main className="w-4/5 min-h-screen bg-gray-100 gap-4 p-6 flex flex-col overflow-auto max-lg:w-full">
       <Header
